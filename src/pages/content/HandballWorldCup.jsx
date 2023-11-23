9b6be9800497378b25ebf4e00c2ebd876b8113b0
gorangg <gorangajsek22@gmail.com>
2023-11-23 02:26:17 +0100

    blog - pages changes

--- src/pages/content/HandballWorldCup.jsx
+++ src/pages/content/HandballWorldCup.jsx
@@ -1,80 +1,83 @@
 import React from 'react';
 import Layout from '../../fragments/Layout';
+import styles from './Content.module.css';
 
 const HandballWorldCup = () => {
 	return (
 		<Layout>
-			<h1>
-				Egypt 2021 World Cup: A Showcase of Skill, Tenacity, and Global
-				Sportsmanship
-			</h1>
-			<h2>Introduction</h2>
-			<section>
-				In the thrilling world of handball, the year 2021 marked a pivotal
-				moment as nations converged for the Handball World Cup, creating a
-				spectacle that transcended borders and celebrated the essence of
-				teamwork and skill. Held in Egypt, this tournament was a testament to
-				the enduring spirit of handball enthusiasts worldwide.
-			</section>
-			<h2>Host Country's Embrace</h2>
-			<section>
-				Egypt welcomed teams and fans with open arms, providing a fitting
-				backdrop for the intensity of handball. The host cities pulsated with
-				the rhythm of the game, creating an atmosphere where every pass, goal,
-				and save became a shared experience among fans from diverse corners of
-				the globe.
-			</section>
-			<h2>Unforgettable Matches</h2>
-			<section>
-				The tournament delivered a barrage of gripping matches that showcased
-				the essence of handball—fast-paced action, strategic brilliance, and
-				heart-stopping moments. From group stage clashes to the high-stakes
-				knockout rounds, each match added a chapter to the narrative of the 2021
-				Handball World Cup.
-			</section>
-			<h2>Emergence of New Stars</h2>
-			<section>
-				While established stars dazzled on the court, the World Cup also
-				provided a stage for emerging talents to shine. Young players and rising
-				stars etched their names into the annals of handball history, proving
-				that the sport's future is as bright as its illustrious past.
-			</section>
-			<h2>Diversity in Playing Styles</h2>
-			<section>
-				Handball is a game that embraces diverse playing styles, and the 2021
-				World Cup was a canvas of tactical brilliance. Teams brought their
-				unique strategies to the court, creating a mosaic of playing styles that
-				captivated fans and showcased the versatility of the sport.
-			</section>
-			<h2>Heart-Stopping Finals</h2>
-			<section>
-				As the tournament reached its climax, the finals unfolded with
-				heart-stopping intensity. The championship match between Denmark and
-				Sweden gripped the audience as both teams left their all on the court.
-				In the end, Denmark emerged victorious, claiming the coveted title and
-				etching their names in handball lore.
-			</section>
-			<h2>Legacy of Unity</h2>
-			<section>
-				Beyond the goals and victories, the 2021 Handball World Cup left a
-				lasting legacy of unity. The tournament brought together nations and
-				fans, fostering a sense of sportsmanship and camaraderie that extends
-				beyond the final whistle. Handball, in its purest form, became a medium
-				through which people connected and celebrated a shared love for the
-				game.
-			</section>
-			<h2>Conclusion</h2>
-			<section>
-				As the echoes of the final cheers reverberated through the arenas, the
-				2021 Handball World Cup drew to a close, leaving behind a legacy of
-				unforgettable moments and inspiring performances. In a world that often
-				seeks common ground, handball once again proved its ability to unite,
-				excite, and showcase the very best in athletic prowess. As we reflect on
-				the triumphs and challenges of the 2021 Handball World Cup, one thing is
-				certain—the spirit of handball will continue to thrive, fueled by the
-				passion of players and fans alike, eagerly awaiting the next chapter in
-				the sport's storied history.
-			</section>
+			<div className={styles.blogArticle}>
+				<h1>
+					Egypt 2021 World Cup: A Showcase of Skill, Tenacity, and Global
+					Sportsmanship
+				</h1>
+				<h2>Introduction</h2>
+				<section>
+					In the thrilling world of handball, the year 2021 marked a pivotal
+					moment as nations converged for the Handball World Cup, creating a
+					spectacle that transcended borders and celebrated the essence of
+					teamwork and skill. Held in Egypt, this tournament was a testament to
+					the enduring spirit of handball enthusiasts worldwide.
+				</section>
+				<h2>Host Country's Embrace</h2>
+				<section>
+					Egypt welcomed teams and fans with open arms, providing a fitting
+					backdrop for the intensity of handball. The host cities pulsated with
+					the rhythm of the game, creating an atmosphere where every pass, goal,
+					and save became a shared experience among fans from diverse corners of
+					the globe.
+				</section>
+				<h2>Unforgettable Matches</h2>
+				<section>
+					The tournament delivered a barrage of gripping matches that showcased
+					the essence of handball—fast-paced action, strategic brilliance, and
+					heart-stopping moments. From group stage clashes to the high-stakes
+					knockout rounds, each match added a chapter to the narrative of the
+					2021 Handball World Cup.
+				</section>
+				<h2>Emergence of New Stars</h2>
+				<section>
+					While established stars dazzled on the court, the World Cup also
+					provided a stage for emerging talents to shine. Young players and
+					rising stars etched their names into the annals of handball history,
+					proving that the sport's future is as bright as its illustrious past.
+				</section>
+				<h2>Diversity in Playing Styles</h2>
+				<section>
+					Handball is a game that embraces diverse playing styles, and the 2021
+					World Cup was a canvas of tactical brilliance. Teams brought their
+					unique strategies to the court, creating a mosaic of playing styles
+					that captivated fans and showcased the versatility of the sport.
+				</section>
+				<h2>Heart-Stopping Finals</h2>
+				<section>
+					As the tournament reached its climax, the finals unfolded with
+					heart-stopping intensity. The championship match between Denmark and
+					Sweden gripped the audience as both teams left their all on the court.
+					In the end, Denmark emerged victorious, claiming the coveted title and
+					etching their names in handball lore.
+				</section>
+				<h2>Legacy of Unity</h2>
+				<section>
+					Beyond the goals and victories, the 2021 Handball World Cup left a
+					lasting legacy of unity. The tournament brought together nations and
+					fans, fostering a sense of sportsmanship and camaraderie that extends
+					beyond the final whistle. Handball, in its purest form, became a
+					medium through which people connected and celebrated a shared love for
+					the game.
+				</section>
+				<h2>Conclusion</h2>
+				<section>
+					As the echoes of the final cheers reverberated through the arenas, the
+					2021 Handball World Cup drew to a close, leaving behind a legacy of
+					unforgettable moments and inspiring performances. In a world that
+					often seeks common ground, handball once again proved its ability to
+					unite, excite, and showcase the very best in athletic prowess. As we
+					reflect on the triumphs and challenges of the 2021 Handball World Cup,
+					one thing is certain—the spirit of handball will continue to thrive,
+					fueled by the passion of players and fans alike, eagerly awaiting the
+					next chapter in the sport's storied history.
+				</section>
+			</div>
 		</Layout>
 	);
 };
