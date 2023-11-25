6773d7f30ac36ee6e5df9c07ee8942df0dcd98af
gorangg <gorangajsek22@gmail.com>
2023-11-25 02:37:48 +0100

    blog - changes

--- src/pages/frontpage/Home.jsx
+++ src/pages/frontpage/Home.jsx
@@ -2,32 +2,33 @@ import React from 'react';
 import Layout from '../../fragments/Layout';
 import { Link } from 'react-router-dom';
 import space from '../../components/assets/pictures/space.jpg';
+import styles from './FrontPage.module.css';
 
 const MainPage = () => {
 	return (
 		<Layout>
-			<main>
-				<section>
+			<div className={styles.mainPage}>
+				<section className={styles.firstSection}>
 					<Link to='/footballworldcup'>
 						<img src={space} alt='a' />
 						Qatar 2022 FIFA World Cup: A Spectacle of Soccer in the Desert
 					</Link>
 				</section>
-				<section>
+				<section className={styles.secondSection}>
 					<Link to='/handballworldcup'>
 						<img src={space} alt='a' />
 						Egypt 2021 World Cup: A Showcase of Skill, Tenacity, and Global
 						Sportsmanship
 					</Link>
 				</section>
-				<article>
+				<article className={styles.thirdSection}>
 					<Link to='/lolworlds'>
 						<img src={space} alt='a' />
 						Rifts and Rivalries: The 2022 League of Legends World Championship
 						Unveiled
 					</Link>
 				</article>
-			</main>
+			</div>
 		</Layout>
 	);
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-export { default as Home } from './home/Home';
+export { default as Home } from './frontpage/Home';
 export { default as FootballWorldCup } from './content/FootballWorldCup';
 export { default as HandballWorldCup } from './content/HandballWorldCup';
 export { default as LoLWorlds } from './content/LoLWorlds';
-export { default as About } from './home/About';
+export { default as About } from './frontpage/About';
