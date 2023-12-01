1c99baf6880f098d7445d6264f49525f917d65bc
gorangg <gorangajsek22@gmail.com>
2023-12-01 02:30:56 +0100

    blog - letter G border

--- src/pages/frontpage/Home.jsx
+++ src/pages/frontpage/Home.jsx
@@ -105,7 +105,7 @@ const MainPage = () => {
 				</div>
 				<div>
 					<div className={styles.letterNav}>
-						<div>
+						<div className={styles.borderLetterG}>
 							<a href='#top' className={styles.letterG}>
 								G
 							</a>
