79597ad01e5b8d74cb11efbf941326fb10dfbe2e
gorangg <gorangajsek22@gmail.com>
2023-12-03 23:16:45 +0100

    blog - scrolling fixed

--- src/fragments/Layout.jsx
+++ src/fragments/Layout.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import ScrollButton from '../components/ScrollButton/ScrollButton.jsx';
+import ScrollToTop from '../components/ScrollToTop/ScrollToTop.jsx';
 
 const Layout = (props) => {
 	return (
 		<div className='container'>
+			<ScrollToTop />
 			<Header />
 			<div className='mainContainer'>
 				<main className='main'>{props.children}</main>
